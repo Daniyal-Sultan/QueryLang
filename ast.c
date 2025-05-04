9a673461809db8c458d35b7b2ed9556ac26644fe
Daniyal-Sultan <113551325+Daniyal-Sultan@users.noreply.github.com>
2025-05-04 12:12:03 +0500

    Update ast.c

--- ast.c
+++ ast.c
@@ -1,5 +1,21 @@
 #include "ast.h"
 #include <stdlib.h>
+#include <string.h>
+
+static AstNode *ast_root = NULL;
+
+AstNode* ast_create_operation_list(AstNode *op) {
+    return op;
+}
+
+AstNode* ast_append_operation(AstNode *list, AstNode *op) {
+    AstNode *current = list;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = op;
+    return list;
+}
 
 AstNode* ast_create_select(char **columns) {
     AstNode *node = malloc(sizeof(AstNode));
@@ -8,4 +24,9 @@ AstNode* ast_create_select(char **columns) {
     node->next = NULL;
     return node;
 }
-// ... Implement other node creators
+
+/* Implement other creation functions similarly */
+
+AstNode* get_ast_root(void) {
+    return ast_root;
+}
