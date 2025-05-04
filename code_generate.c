6e26c09e9fe45d7be3021d31cd1f84467294ffe2
Daniyal-Sultan <113551325+Daniyal-Sultan@users.noreply.github.com>
2025-05-04 12:13:30 +0500

    Update code_generate.c

--- code_generate.c
+++ code_generate.c
@@ -3,17 +3,23 @@
 
 void generate_python(AstNode *root) {
     printf("import pandas as pd\n\ndf = pd.read_csv('input.csv')\n");
+    
     while (root != NULL) {
         switch (root->type) {
-            case NODE_SELECT:
-                // Generate SELECT code
-                printf("df = df[%s]\n", (char*)root->data);
+            case NODE_SELECT: {
+                char **cols = (char**)root->data;
+                printf("df = df[[");
+                for (int i = 0; cols[i] != NULL; i++) {
+                    printf("'%s'%s", cols[i], cols[i+1] ? ", " : "");
+                }
+                printf("]]\n");
                 break;
-            case NODE_FILTER:
-                // Generate FILTER code
-                printf("df = df[df['%s'] > %d]\n", ...);
+            }
+            case NODE_FILTER: {
+                // Implement filter code generation
                 break;
-            // ... Handle other nodes
+            }
+            // Add other cases
         }
         root = root->next;
     }
